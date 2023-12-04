c99b074727018907f58a722dae95367d191017b2
nanguaguag <2022181640245@stu.scu.edu.cn>
2023-12-04 21:36:18 +0800

    设置静态资源

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = 'abcdefghijklmn'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)  # cookies在7天后过期
-app.config['STATIC_URL_PATH'] = '/'
